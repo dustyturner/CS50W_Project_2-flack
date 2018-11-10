8de0287efa1a91df7fc6d3e6afd6ea60c80751d5
dustyturner <relinsface@DESKTOP-17AS9GI.localdomain>
2018-11-10 16:22:25 +0000

    Forms - disabled submit when empty and now autoclears on submit

--- static/index.js
+++ static/index.js
@@ -41,16 +41,38 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         });
 
+        // Button begins disabled by default
+        document.querySelector('#submit_message').disabled = true
+        // Enable button only if there is text in the input field
+        document.querySelector('#send_message').onkeyup = () => {
+            if (document.querySelector('#message').value.length > 0)
+                document.querySelector('#submit_message').disabled = false;
+            else
+                document.querySelector('#submit_message').disabled = true;
+        };
+
         document.querySelector('#send_message').onsubmit = () => {
             const message = document.querySelector('#message').value;
             socket.emit('send message', message);
+            // Clear input field and disable button again
+            document.querySelector('#message').value = '';
+            document.querySelector('#submit_message').disabled = true;
             return false;
+        };
+
+        // Button begins disabled by default
+        document.querySelector('#submit_channel').disabled = true
+        // Enable button only if there is text in the input field
+        document.querySelector('#create_channel').onkeyup = () => {
+            if (document.querySelector('#name').value.length > 0)
+                document.querySelector('#submit_channel').disabled = false;
+            else
+                document.querySelector('#submit_channel').disabled = true;
         };
 
         document.querySelector('#create_channel').onsubmit = () => {
             const name = document.querySelector('#name').value;
             socket.emit('create channel', name)
-            socket.emit('join channel', name)
             window.location.reload();
         };
     });
